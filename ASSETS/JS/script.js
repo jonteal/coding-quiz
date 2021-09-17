689ff5832615c4b093bb0ee44f370c937b3a3ea7
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-16 20:58:49 -0500

    Added CSS and JS pages

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,13 @@
+// Assign variables to all components that will change throughout the quiz
+
+// Pseudo code the quiz before really focusing on the JS
+
+// 
+
+// Create questions as objects
+{
+    question: "Sample answer 1",
+    choice1: ""
+    
+}
+
