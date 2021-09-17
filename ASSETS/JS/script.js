3f20ce1f70b2716158478d71880c52e0707cf0ae
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-16 22:42:11 -0500

    Added some basic elements and CSS

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -5,9 +5,30 @@
 // 
 
 // Create questions as objects
-{
-    question: "Sample answer 1",
-    choice1: ""
+// {
+//     // question: "Sample answer 1",
+//     // choice1: ""
     
-}
+// }
 
+
+// Timer 
+var timeEl = document.querySelector(".time");
+
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds remaining.";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    
+
+    }, 1000);
+}
